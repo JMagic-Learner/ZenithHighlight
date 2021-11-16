7cd5f4b768676027f4c9b12c377684cd4df1054e
JasonDesktop <MaJason93@gmail.com>
2021-11-15 21:39:41 -0800

    Deployed to Heroku, testing images

---
+++ src/pages/Article1.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
+
+export default function Articles() {
+    return(
+     
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography varaint="h4"> Title </Typography>
+          </Box>
+           
+        );
+      }
