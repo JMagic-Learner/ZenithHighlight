dfdfbf5b8dd51906e70c42c16a286e97e61a1f0c
JasonDesktop <MaJason93@gmail.com>
2021-12-06 23:57:04 -0800

    removing carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,7 +10,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
-import Carousel1 from '../components/Carousel';
+// import Carousel1 from '../components/Carousel';
 
 
 
