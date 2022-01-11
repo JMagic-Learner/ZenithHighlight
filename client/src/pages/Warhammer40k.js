9a4f24c931420ac31f8fe2c7ac63e7e8339307f8
JasonDesktop <MaJason93@gmail.com>
2022-01-10 22:55:42 -0800

    We are adding dropdown menus

--- client/src/pages/Warhammer40k.js
+++ client/src/pages/Warhammer40k.js
@@ -4,14 +4,19 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { styled } from '@mui/material/styles';
-import CardMedia from '@mui/material/CardMedia';
+
 import Grid from '@mui/material/Grid';
-import { useParams } from 'react-router-dom';
+
 import Paper from '@mui/material/Paper';
 import { useQuery } from '@apollo/client';
 import { QUERY_OBJECTIVES } from '../utils/queries';
 import { QUERY_NAME_OBJECTIVES } from '../utils/queries';
 import { QUERY_CAT_OBJECTIVES } from '../utils/queries';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
@@ -24,6 +29,12 @@ export default function Warhammer() {
   if (data) {
     console.log('We have succesfully queried objectives');
   }
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   // Load all Warhammer 40k 2020 Objectives into this array.
   const ObjectivesArray = data?.objectives || [];
   console.log(ObjectivesArray);
@@ -37,10 +48,71 @@ export default function Warhammer() {
   // }
 
   return(
+    
 
     <Box sx={{ flexGrow: 1 }}>
       <Typography sx={{ p: 2.0 }} variant="h2"> 40k Objectives</Typography>
       <Grid container spacing={2} style={{ display: "flex", justifyContent: "flex-start" }}>
+
+        <CardContent>
+          <Item>
+            <Typography> Objective 1 </Typography>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Item>
+        </CardContent>
+
+        <CardContent>
+          <Item>
+            <Typography> Objective 2 </Typography>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Item>
+        </CardContent>
+
+        <CardContent>
+          <Item>
+            <Typography> Objective 3 </Typography>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Item>
+        </CardContent>
 
      
         <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
@@ -73,55 +145,4 @@ export default function Warhammer() {
     </Box>
      
   );
-    // return(
-    //     <Box sx={{ flexGrow: 1 }}>
-          
-    //       <Typography sx={{ p: 2.0 }} variant="h2"> Warhammer 40k - 9th Edition</Typography>
-          
-    //     <Card>
-    //     <CardMedia
-    //             component="img"
-    //             height="550"
-    //             image="./assets/images/DeathCompany3.jpg"
-    //             alt="Death Company"
-    //           /> 
-    //           <CardContent>
-    //             <Typography variant="h6">
-    //              Death Company - Warhammer 40k
-    //             </Typography>
-    //           </CardContent>
-    //            </Card>
-
-    //         <Typography variant="h6">
-    //           Released in August 2020, the 9th iteration of Warhammer 40k was designed to uproot problems 
-    //         </Typography>
-    //         <Grid container spacing={2}>
-    //         <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
-    //           {ObjectivesArray.map((element) => {
-    //           if(element) {
-    //           console.log("element has been detected")
-    //           }
-    //           if(element.name) {
-    //             console.log("A objective has been detected");
-    //             console.log("The name of this objective is: " + element.name);
-    //           }
-    //           return(
-    //             <Item>
-    //             <Typography> {element.category} </Typography>
-    //             <Typography> {element.name} </Typography>
-    //             <Typography> {element.priority} </Typography>
-    //             <Typography> {element.priorityDescription} </Typography>
-    //             <Typography> {element.description} </Typography>
-    //             {ObjectivesArray.victoryPoints.map((VP) => {
-    //               <Typography> {VP} </Typography>
-    //             })}
-    //             </Item>
-    //           );
-    //               })
-    //             }
-    //           </Grid>
-    //           </Grid>
-        
-    //     </Box>
-    // )
-}
+        }
