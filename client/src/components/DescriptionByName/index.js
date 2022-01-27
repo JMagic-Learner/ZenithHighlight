d6cdfd56641ea8bd988d09212dbdedfe9df4ec61
JasonDesktop <MaJason93@gmail.com>
2022-01-27 00:42:59 -0800

    Testing

--- client/src/components/DescriptionByName/index.js
+++ client/src/components/DescriptionByName/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_NAME_OBJECTIVES } from '../utils/queries';
+import { QUERY_NAME_OBJECTIVES } from '../../utils/queries';
 import { Typography } from '@mui/material';
 
 export default function DescriptionByName() {
