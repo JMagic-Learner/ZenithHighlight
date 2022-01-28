511b14fa898cdff914834b8cbd33c86eaa776ea9
JasonDesktop <MaJason93@gmail.com>
2022-01-27 17:43:13 -0800

    This is a test to see if word wrap works inside the child MENUITEM

--- client/src/pages/Warhammer40k.js
+++ client/src/pages/Warhammer40k.js
@@ -28,7 +28,7 @@ export default function Warhammer() {
   let objectivename2 = "";
   let objectivename3 = "";
 
-  
+
   const { loading, data } = useQuery(QUERY_OBJECTIVES);
   if (data) {
     console.log('We have succesfully queried objectives');
@@ -58,12 +58,12 @@ export default function Warhammer() {
     console.log(objectivename3);
     return objectivename3;
   };
- 
+
   // Load all Warhammer 40k 2020 Objectives into this array.
-  
+
   const ObjectivesArray = data?.objectives || [];
-  
-  
+
+
   // const { loading1, objectiveNameFound } = useQuery(QUERY_NAME_OBJECTIVES);
   // if (objectiveNameFound) {
   //   console.log('We have succesfully queried objective by name');
@@ -73,163 +73,163 @@ export default function Warhammer() {
   //   console.log('We have succesfully queried objective by category');
   // }
 
-  return(
-    
+  return (
+
 
     <Box sx={{ flexGrow: 1 }}>
       <Typography sx={{ p: 2.0 }} variant="h2"> 40k Objectives</Typography>
       <Grid container spacing={2} style={{ display: "flex", justifyContent: "flex-start", wordWrap: 'break-word' }}>
 
-      <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
-        <CardContent>
-          <Item>
-            <Typography> Objective 1 </Typography>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Objective 1</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={objective1}
-                label="Objective 1"
-                onChange={handleChange1}
-               
-              >
-                {ObjectivesArray.map((element) => {
-                   if(element) {
-                    console.log("element has been detected")
-                  } 
-                  return(
-                <MenuItem value={element.name}>
-                   <CardContent>
-          <Item>
-          <Typography> {element.category} </Typography>
-                <Typography>Objective: {element.name} </Typography>
-                <Typography>Priority: {element.priority} </Typography>
-                <Typography>When to score: {element.priorityDescription} </Typography>
-                <Typography>Description {element.description} </Typography>
-          </Item>
-          </CardContent>
-                </MenuItem>
-                  );
-                })}
-              
-              </Select>
-            </FormControl>
-            
-          </Item>
-        </CardContent>
-    </Grid>
-
- 
-
-    <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
-        <CardContent>
-          <Item>
-            <Typography> Objective 2 </Typography>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Objective 2</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={objective2}
-                label="Objective 2"
-                onChange={handleChange2}
-              >
-               {ObjectivesArray.map((element) => {
-                   if(element) {
-                    console.log("element has been detected")
-                  } 
-                  return(
-                <MenuItem value={element.name}>
-                  <CardContent>
-          <Item>
-          <Typography> {element.category} </Typography>
-                <Typography>Objective: {element.name} </Typography>
-                <Typography>Priority: {element.priority} </Typography>
-                <Typography>When to score: {element.priorityDescription} </Typography>
-                <Typography >Description {element.description} </Typography>
-          </Item>
-          </CardContent>
-                </MenuItem>
-                  );
-                })}
-              
-              </Select>
-            </FormControl>
-          </Item>
-        </CardContent>
-    </Grid>
-
-
-
-    <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
-        <CardContent>
-          <Item>
-            <Typography> Objective 3 </Typography>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Objective 3</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={objective3}
-                label="Objective 3"
-                onChange={handleChange3}
-              >
-               {ObjectivesArray.map((element) => {
-                   if(element) {
-                    console.log("element has been detected")
-                  } 
-                  return(
-                <MenuItem value={element.name}>{element.name}
-                <CardContent>
-                <Item>
-                <Typography> {element.category} </Typography>
-                      <Typography>Objective: {element.name} </Typography>
-                      <Typography>Priority: {element.priority} </Typography>
-                      <Typography>When to score: {element.priorityDescription} </Typography>
-                      <Typography>Description {element.description} </Typography>
-                </Item>
-                </CardContent>
-                </MenuItem>
-                  );
-                })}
-              
-              </Select>
-            </FormControl>
-          </Item>
-        </CardContent>
-    </Grid>
-
-     
         <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
-        {ObjectivesArray.map((element) => {
-        if(element) {
-        console.log("element has been detected")
-        }
-        
-        return(
-          
-      <CardContent>
-          <Item>
-          <Typography> {element.category} </Typography>
-                <Typography>Objective: {element.name} </Typography>
-                <Typography>Priority: {element.priority} </Typography>
-                <Typography>When to score: {element.priorityDescription} </Typography>
-                <Typography>Description {element.description} </Typography>
-          </Item>
+          <CardContent>
+            <Item>
+              <Typography> Objective 1 </Typography>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Objective 1</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={objective1}
+                  label="Objective 1"
+                  onChange={handleChange1}
+
+                >
+                  {ObjectivesArray.map((element) => {
+                    if (element) {
+                      console.log("element has been detected")
+                    }
+                    return (
+                      <MenuItem value={element.name} style={{wordWrap: 'break-word'}}>
+                        <CardContent>
+                          <Item>
+                            <Typography> {element.category} </Typography>
+                            <Typography>Objective: {element.name} </Typography>
+                            <Typography>Priority: {element.priority} </Typography>
+                            <Typography>When to score: {element.priorityDescription} </Typography>
+                            <Typography>Description {element.description} </Typography>
+                          </Item>
+                        </CardContent>
+                      </MenuItem>
+                    );
+                  })}
+
+                </Select>
+              </FormControl>
+
+            </Item>
           </CardContent>
-        );
-            })
+        </Grid>
+
+
+
+        <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
+          <CardContent>
+            <Item>
+              <Typography> Objective 2 </Typography>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Objective 2</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={objective2}
+                  label="Objective 2"
+                  onChange={handleChange2}
+                >
+                  {ObjectivesArray.map((element) => {
+                    if (element) {
+                      console.log("element has been detected")
+                    }
+                    return (
+                      <MenuItem value={element.name}>
+                        <CardContent>
+                          <Item>
+                            <Typography> {element.category} </Typography>
+                            <Typography>Objective: {element.name} </Typography>
+                            <Typography>Priority: {element.priority} </Typography>
+                            <Typography>When to score: {element.priorityDescription} </Typography>
+                            <Typography >Description {element.description} </Typography>
+                          </Item>
+                        </CardContent>
+                      </MenuItem>
+                    );
+                  })}
+
+                </Select>
+              </FormControl>
+            </Item>
+          </CardContent>
+        </Grid>
+
+
+
+        <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
+          <CardContent>
+            <Item>
+              <Typography> Objective 3 </Typography>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Objective 3</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={objective3}
+                  label="Objective 3"
+                  onChange={handleChange3}
+                >
+                  {ObjectivesArray.map((element) => {
+                    if (element) {
+                      console.log("element has been detected")
+                    }
+                    return (
+                      <MenuItem value={element.name}>
+                        <CardContent>
+                          <Item>
+                            <Typography> {element.category} </Typography>
+                            <Typography>Objective: {element.name} </Typography>
+                            <Typography>Priority: {element.priority} </Typography>
+                            <Typography>When to score: {element.priorityDescription} </Typography>
+                            <Typography>Description {element.description} </Typography>
+                          </Item>
+                        </CardContent>
+                      </MenuItem>
+                    );
+                  })}
+
+                </Select>
+              </FormControl>
+            </Item>
+          </CardContent>
+        </Grid>
+
+
+        <Grid item xs={12} sm={6} md={8} sx={{ m: 'auto' }}>
+          {ObjectivesArray.map((element) => {
+            if (element) {
+              console.log("element has been detected")
+            }
+
+            return (
+
+              <CardContent>
+                <Item>
+                  <Typography> {element.category} </Typography>
+                  <Typography>Objective: {element.name} </Typography>
+                  <Typography>Priority: {element.priority} </Typography>
+                  <Typography>When to score: {element.priorityDescription} </Typography>
+                  <Typography>Description {element.description} </Typography>
+                </Item>
+              </CardContent>
+            );
+          })
           }
         </Grid>
-    
-        
-        
-        
+
+
+
+
       </Grid>
-    
+
     </Box>
-     
+
   );
-        }
+}
       
