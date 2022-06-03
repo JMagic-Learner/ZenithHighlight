9a984b239e1086ca935393821d56a7659a8714ad
JasonDesktop <MaJason93@gmail.com>
2022-06-03 11:07:44 -0700

    Figuring out the connection error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-'mongodb://localhost/27017' ||  process.env.MONGODB_URI ,
+'mongodb://localhost/zenithhighlight' ||  process.env.MONGODB_URI ,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
