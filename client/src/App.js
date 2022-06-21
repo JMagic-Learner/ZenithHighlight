72607996c691783b59e6b2d70290669c1e69ed66
JasonDesktop <MaJason93@gmail.com>
2022-06-21 01:01:53 -0700

    this is a test

--- client/src/App.js
+++ client/src/App.js
@@ -81,7 +81,7 @@ function App() {
     <div className="App">
       <div className="banner">
         <img className="logo" src={Logo}/>
-    <Typography variant="h2">
+    <Typography variant="h2" className="Banner-Header">
       ZENITH HIGHLIGHT
       </Typography>
       </div>
