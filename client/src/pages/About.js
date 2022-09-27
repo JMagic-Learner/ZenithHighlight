8b271834f0ebb37705b165427d756684358451e4
JasonDesktop <MaJason93@gmail.com>
2022-09-26 18:14:14 -0700

    Revert "redoing the about page"
    This reverts commit 9711388f24e9de192cae85c8a79bd0ab55938e1b.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,17 +5,12 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-import Carousel from "../components/Carousel"
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+//Image Components
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 
-import ListItemText from '@mui/material/ListItemText';
-
-import Link from '@mui/material/Link';
-import Button from '@mui/material/Button';
-
-import image1 from '../assets/images/DaofeiExample.jpeg'
 
 const buttonStyle = createTheme({
   palette: {
@@ -35,83 +30,92 @@ const buttonStyle = createTheme({
 });
 
 const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+  let picturePosition = 0;
+  let pictureArray = [
+    '../assets/images/BloodAngelsLineup.jpg', 
+    '../assets/images/AssaultIntercessor.jpg', 
+    '../assets/images/yujing.jpg', 
+    '../assets/images/TifaExpo.jpeg', 
+    "../assets/images/ParadeMortisContemptor2.jpg"
+    ];
 
 export default function About() {
-
-  return (
-
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item sx={8} md={12} lg={12}>
-          <div className="margin-top-1">
-          <Typography   variant="h2" align='center'>
-            Welcome to Zenith Highlight
-          </Typography>
-          </div>
  
+
+  
+let [pictureState, pictureDisplay] = React.useState("../assets/images/BloodAngelsLineup.jpg");
+
+// M<Y OWN UNIQUE CODE FOR REACTF CAROUSEL
+let scrollForward =async (event) => {
+  if (picturePosition > pictureArray.length - 1) {
+    picturePosition=0;
+   }
+  console.log( pictureArray[picturePosition]);
+  pictureState = pictureDisplay(pictureArray[picturePosition]);
+  picturePosition++;
+  
+};
+
+let scrollBack =async (event) => {
+  if (picturePosition < 0) {
+    picturePosition=pictureArray.length - 1;
+   }
+  console.log( pictureArray[picturePosition]);
+  pictureState = pictureDisplay(pictureArray[picturePosition]);
+  picturePosition--;
+  
+
+};
+    return(
+        
+        <Box sx={{ flexGrow: 1 }}>
+       <Grid container spacing={2} align="center" >
+       <Grid item sx={8} md={12} lg={12}>
+        <Typography variant="h3" align='center'>
+           Welcome to Zenith Highlight 
+        </Typography>
         </Grid>
+        <Grid item sx={12} md={12} lg={12}>
+ 
+        
+        <Card  >
+       <CardMedia
+        component="img"
+        height="600"
+        image={pictureState}
+        alt="a list of pictures"
+      /> 
+    </Card>
+    < ArrowBackIosIcon variant="contained" onClick={scrollForward} sx={{ fontSize: 50 }}   > Scroll Through Forwards</ArrowBackIosIcon>
+    < ArrowForwardIosIcon variant="contained" onClick={scrollBack} sx={{ fontSize: 50 }} > Scroll Through BAckwards </ ArrowForwardIosIcon>
 
+        </Grid>
+       
+        <Grid item sx={12} md={12} lg={12}>
+        <Typography variant="h8" align="center">
+            <p>ZenithHighlight is a hobby website that is designed first and foremost for wargamers</p>
+            <p>One of the things that we noticed in the tabletop wargaming hobby, is the lack of digital implementation</p>
+            <p>To that end, Zenith Highlight aims to address three goals</p>
+            
+            <li> 1.) Translate text to digital mediums</li>
+            <li> 2.) Be open resource for wargamers to utilize</li>
+            <li> 3.) Showcase models, wargames and ideas from many wargame communities</li>
 
+            <p> One of my current goals is to maintain an database of 40k 9th Edition objectives</p>
+            <p> This initial goal transformed into making a Victory point tracker </p>
+         
+        </Typography>
+        </Grid>
+        </Grid>   
+      </Box>
 
-        <Box className="text-align-left about-paragraph margin-top-1">
-          <img className="about-image" src={image1}/>
-          <div>
-          <Typography variant="h8">
-          ZenithHighlight is a hobby website that is designed to showcase painting and provide wargame utilties to hobbyists. Users can sign up, login, and post their articles regarding the wargaming hobby. Furthermore, users can utilize the 40k 9th Edition scoring utility to help keep track of 9th Edition Matched Play matches.
-          Overall, Zenith Highlight aims to continue providing and developing digital applications for public and open source use. 
-          </Typography>
-          <br></br>   <br></br>
-          <Typography variant="h8">
-         Zenith Highlight currently showcases the following hobbies;
-
-          </Typography>
-          
-          <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <Button component={Link } href="https://warhammer40000.com/"><ListItemText primary="Warhammer 40k" /></Button>
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-            <Button component={Link } href="https://kingdomdeath.com/"><ListItemText primary="Kingdom Death" /></Button>
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <Button component={Link } href="https://www.catalystgamelabs.com/brands/battletech"> <ListItemText primary="Battletech" /></Button>
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-                <Button component={Link } href="https://infinitytheuniverse.com/games/infinity"><ListItemText primary="Infinity"/></Button>
-            </ListItemButton>
-          </ListItem>
-        </List>
-          <Typography variant="h8">
-          Zenith Highlight was started in 2019 using traditional HTML, CSS, and Javascript. 
-          This website was later reinitialized using the React, MongoDB, Express and Node.JS in order to modernize the UI and to provide greater utility. 
-          Future plans include adding and expanding upon the Gallery, and improving on the existing Carousel component found within the Client directory
-          </Typography>
-          </div>
-        </Box>
-
-        {/* <Grid item sx={12} md={12} lg={12}>
-
-          <Carousel/>
-
-        </Grid> */}
-
-
-      </Grid>
-    </Box>
-
-
-  );
+        
+    );
 }
 
